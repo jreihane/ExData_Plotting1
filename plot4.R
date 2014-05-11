aaee71ca6655f84d0a65dc1afd2e99da20b03a00
reihane zekri <reihane.zekri@gmail.com>
2014-05-12 01:20:35 +0430

    2

--- plot4.R
+++ plot4.R
@@ -1,8 +1,8 @@
 setwd("G:\\Projects\\R\\Exploratory\\project1\\ExData_Plotting1")
 
-dataFile <- read.table("h1.txt", sep=";", header=T)
+dataFile <- read.table("h1.txt", sep=";", header=T,as.is=TRUE)
 dataFile$Date <- as.Date(dataFile$Date,format="%d/%m/%Y")
-d1 <- paste(dataFile$Date,strptime(dataFile$Time,format="%H:%M:%S"))
+d1 <- paste(dataFile$Date,dataFile$Time)
 dataFile$DateTime <- as.POSIXct(d1)
 
 par(mfrow=c(2,2))
@@ -12,5 +12,14 @@ with(dataFile, plot(Global_active_power,type="l",xlab="", ylab="Global Active Po
 
 
 with(dataFile, plot(Voltage,type="l",xlab="", ylab="Voltage"))
-with(dataFile, plot(Sub_metering_1,type="l",xlab="", ylab="Energy Sub metering",xlim=c(min(DateTime),max(DateTime))))
+
+with(dataFile,plot(DateTime, Sub_metering_1,type="l", xaxt="n",ylab="Energy Submetering",xlab=""))
+with(dataFile, points(DateTime,Sub_metering_2,col="red",type="l"))
+with(dataFile, points(DateTime,Sub_metering_3,col="blue",type="l"))
+
+mini <- min(dataFile$DateTime)
+axis(side=1, at=c(mini, mini+86400, mini+2*86400),labels=c("Thu","Fri","Sat"))
+legend("topright",legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"),lty=c(1,1),col=c("black","red","blue"),seg.len=.5)
+
+
 with(dataFile, plot(Global_reactive_power,type="l"))
