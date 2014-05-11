3f0e967dc99c543f14ba719e14bc871f2055fbf6
reihane zekri <reihane.zekri@gmail.com>
2014-05-12 01:22:33 +0430

    3

--- plot4.R
+++ plot4.R
@@ -7,12 +7,13 @@ dataFile$DateTime <- as.POSIXct(d1)
 
 par(mfrow=c(2,2))
 
-
+# row = 1 column = 1
 with(dataFile, plot(Global_active_power,type="l",xlab="", ylab="Global Active Power"))
 
-
+# row = 1 column = 2
 with(dataFile, plot(Voltage,type="l",xlab="", ylab="Voltage"))
 
+# row = 2 column = 1
 with(dataFile,plot(DateTime, Sub_metering_1,type="l", xaxt="n",ylab="Energy Submetering",xlab=""))
 with(dataFile, points(DateTime,Sub_metering_2,col="red",type="l"))
 with(dataFile, points(DateTime,Sub_metering_3,col="blue",type="l"))
@@ -21,5 +22,5 @@ mini <- min(dataFile$DateTime)
 axis(side=1, at=c(mini, mini+86400, mini+2*86400),labels=c("Thu","Fri","Sat"))
 legend("topright",legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"),lty=c(1,1),col=c("black","red","blue"),seg.len=.5)
 
-
+# row = 2 column = 2
 with(dataFile, plot(Global_reactive_power,type="l"))
